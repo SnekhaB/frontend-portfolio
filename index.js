239cf3758ebe3a24260b671a70c8adda8b68563d
snekha <snekhabaskar29@gmail.com>
2025-06-12 11:34:16 +0530

    commit changes

---
+++ index.js
@@ -0,0 +1,23 @@
+let navLinks = document.querySelectorAll('nav a');
+
+
+
+let navLinksAttribute = navLinks.getAttribute('data-target')
+
+console.log(navLinks);
+
+
+
+
+
+
+
+
+// let targetAboutSection = document.querySelector('#about-link')
+// let targetProjectsSection = document.querySelector('#projects-link')
+// let targetSkillsSection = document.querySelector('#skills-link')
+// let targetEducationSection = document.querySelector('#education-link')
+
+// targetAboutSection.addEventListener('click',() =>{
+//   targetAboutSection.style.
+// })
